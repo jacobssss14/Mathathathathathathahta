d91a8b11940e21af317a7d8a18f6e1acc3c0a42b
jacobssss14 <jacobssss14@users.noreply.github.com>
2017-09-27 09:17:33 +0800

    Major Bug FIxes
    *Added App Icon
    *Fixed previous app crashes

--- app/src/main/java/io/github/mathrio/matthathathatha/GameScreen.java
+++ app/src/main/java/io/github/mathrio/matthathathatha/GameScreen.java
@@ -10,7 +10,7 @@ import android.widget.EditText;
 public class GameScreen extends AppCompatActivity {
 
 
-    Button btP, btE, bt0, btD, bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9;
+    Button btP, btE, bt0, btD, bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9, btSlash, btDot;
     EditText input1;
 
     @Override
@@ -28,7 +28,7 @@ public class GameScreen extends AppCompatActivity {
         });
         input1 = (EditText) findViewById(R.id.editText1);
         btE = (Button) findViewById(R.id.button);
-        bt0 = (Button) findViewById(R.id.button7);
+        bt0 = (Button) findViewById(R.id.button8);
         bt0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,7 +36,7 @@ public class GameScreen extends AppCompatActivity {
                 input1.setText(input1.getText().insert(input1.getText().length(),"0"));
             }
         });
-        btD = (Button) findViewById(R.id.button8);
+        btD = (Button) findViewById(R.id.button9);
         btD.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,7 +44,7 @@ public class GameScreen extends AppCompatActivity {
                 input1.setText(input1.getText().delete(input1.getText().length()-1,input1.getText().length()));
             }
         });
-        bt1 = (Button) findViewById(R.id.button11);
+        bt1 = (Button) findViewById(R.id.button10);
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,7 +52,7 @@ public class GameScreen extends AppCompatActivity {
                 input1.setText(input1.getText().insert(input1.getText().length(), "1"));
             }
         });
-        bt2 = (Button) findViewById(R.id.button12);
+        bt2 = (Button) findViewById(R.id.button11);
         bt2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,7 +60,7 @@ public class GameScreen extends AppCompatActivity {
                 input1.setText(input1.getText().insert(input1.getText().length(), "2"));
             }
         });
-        bt3 = (Button) findViewById(R.id.button13);
+        bt3 = (Button) findViewById(R.id.button12);
         bt3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -73,7 +73,7 @@ public class GameScreen extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                input1.setText(input1.getText().insert(input1.getText().length(), "4"));
+                input1.setText(input1.getText().insert(input1.getText().length(), "6"));
             }
         });
         bt5 = (Button) findViewById(R.id.button15);
@@ -89,10 +89,10 @@ public class GameScreen extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                input1.setText(input1.getText().insert(input1.getText().length(), "6"));
+                input1.setText(input1.getText().insert(input1.getText().length(), "4"));
             }
         });
-        bt7 = (Button) findViewById(R.id.button18);
+        bt7 = (Button) findViewById(R.id.button17);
         bt7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -100,7 +100,7 @@ public class GameScreen extends AppCompatActivity {
                 input1.setText(input1.getText().insert(input1.getText().length(), "7"));
             }
         });
-        bt8 = (Button) findViewById(R.id.button19);
+        bt8 = (Button) findViewById(R.id.button18);
         bt8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -108,7 +108,7 @@ public class GameScreen extends AppCompatActivity {
                 input1.setText(input1.getText().insert(input1.getText().length(), "8"));
             }
         });
-        bt9 = (Button) findViewById(R.id.button21);
+        bt9 = (Button) findViewById(R.id.button19);
         bt9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -117,6 +117,21 @@ public class GameScreen extends AppCompatActivity {
             }
         });
 
+        btDot = (Button) findViewById(R.id.button7);
+        btDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input1.setText(input1.getText().insert(input1.getText().length(), "."));
+            }
+        });
+
+        btSlash = (Button) findViewById(R.id.button13);
+        btSlash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input1.setText(input1.getText().insert(input1.getText().length(), "/"));
+            }
+        });
 
 
 
